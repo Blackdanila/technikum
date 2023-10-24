b9aa6f7d5242140ccd818c33dfa2b03ec3adb7d5
SGrechnev <sgrechnev@gmail.com>
2023-10-24 10:38:17 +0700

    Реализован работающий с wiremock тест

---
+++ src/main/java/ru/nspk/technikum/demo/models/Sale.java
@@ -0,0 +1,12 @@
+package ru.nspk.technikum.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sale {
+    private int amount;
+}
--- src/test/java/ru/nspk/technikum/demo/test/SaleTest.java
+++ src/test/java/ru/nspk/technikum/demo/test/SaleTest.java
@@ -1,6 +1,23 @@
 package ru.nspk.technikum.demo.test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import ru.nspk.technikum.demo.models.Client;
+import ru.nspk.technikum.demo.models.Sale;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -19,20 +36,87 @@ import static org.hamcrest.Matchers.equalTo;
  */
 public class SaleTest {
 
-    private int getDiscount(int id) {
+    WireMock wireMock = new WireMock("http", "localhost", 8080);
 
-        int salesCount = 0; // здесь запрос в мок
+    ObjectMapper objectMapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+
+    OkHttpClient httpClient = new OkHttpClient();
+
+    @BeforeEach
+    public void setupWireMock() {
+        this.wireMock.register(
+                WireMock.get(WireMock.urlPathMatching("/client/([0-9]+)"))
+                        .willReturn(ResponseDefinitionBuilder.okForJson(
+                                new Client(
+                                        "Ivan",
+                                        "Ivanov",
+                                        LocalDate.of(1970, 6, 1),
+                                        List.of(
+                                                new Sale(1),
+                                                new Sale(2),
+                                                new Sale(3),
+                                                new Sale(4),
+                                                new Sale(5),
+                                                new Sale(6)
+                                        )
+                                )
+                        ))
+                        .atPriority(1000)
+        );
+    }
+
+    private int getDiscount(int id) {
+        int salesCount = getSalesCount(id); // здесь запрос в мок
         // Больше пяти заказов, скидка 10%
         return salesCount > 5 ? 10 : 0;
+    }
+
+    private int getSalesCount(int id) {
+        int salesCount = 0;
+
+        Request request = new Request.Builder().
+                url("http://localhost:8080/client/"+id)
+                .get()
+                .build();
+        Call call = httpClient.newCall(request);
+        try (Response response = call.execute()){
+            var body = response.body();
+            if (body != null) {
+                var client = objectMapper.readValue(response.body().bytes(), Client.class);
+                salesCount = client == null ? 0 : client.getSales().size();
+            }
+        } catch (IOException e) {
+            System.out.println("[DEBUG] " + e.getMessage());
+            System.out.println("Service unavailable. No discount.");
+        }
+
+        return salesCount;
     }
 
     @Test
     public void getDiscountTest() {
         // Значение предустановленное в моке соответствует скидке 10%
         assertThat(10, equalTo(getDiscount(123)));
+
         // меняем историю покупок клиента в моке
+        var stub = this.wireMock.register(
+                WireMock.get(WireMock.urlMatching("/client/([0-9]+)"))
+                        .willReturn(ResponseDefinitionBuilder.okForJson(
+                                new Client(
+                                        "Ivan",
+                                        "Ivanov",
+                                        LocalDate.of(1970, 6, 1),
+                                        List.of(new Sale(1))
+                                )
+                        ))
+                        .atPriority(1)
+        );
+
         assertThat(0, equalTo(getDiscount(123)));
+
         // меняем историю покупок клиента на исходную
+        this.wireMock.removeStubMapping(stub);
+
         assertThat(10, equalTo(getDiscount(123)));
     }
 }
